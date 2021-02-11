6ffe59eb51242fcaee2cb82433db9f2d7cbb3b36
MaagMichael <63949690+MaagMichael@users.noreply.github.com>
2021-02-11 18:14:06 +0100

    shiji's new simple gui

---
+++ Child_Health_Monitor_new.py
@@ -0,0 +1,129 @@
+from tkinter import *
+# imports every exposed object in Tkinter 
+#Tkinter is the standard GUI library for Python.
+
+def calc_BMI ():
+    pass
+
+def input_value():
+    #creating basic Tkinter window
+    window= Tk()
+    window.geometry("750x400")
+    #setting the default window size using geometry function. size of the window width=750 and height =400
+
+    window.resizable(0,0)
+    #this prevents from resizing the window
+
+    window.title("Child Health Monitor")
+    #giving title for the window
+
+    #**************************************************************
+    # Age in years
+
+    Agelabel=StringVar()
+    #value holder for string variables
+
+    Agelabel.set("Age")
+    #set the variable to the VALUE
+
+    Agetitle=Label(window, textvariable=Agelabel,bg="yellow",fg="green",font=("Calibri",20))
+
+    Agetitle.grid(row=0, column=0)
+    #grid uses the matrix row column concepts to organize the widgets
+
+    Age= IntVar() 
+    #value holder for integer variables
+
+    spinlength= Spinbox(window, from_=1, to_=14, textvariable=Age, width=13,font=("Calibri",20))
+
+    spinlength.grid(row=0,column=1)
+
+
+
+    #*****************************************************************
+    # Gender male, female, no say
+
+    genderlabel=StringVar()
+    #value holder for string variables
+
+    genderlabel.set("Gender:")
+    #set the variable to the VALUE
+
+    gendertitle=Label(window, textvariable=genderlabel,bg="yellow",fg="green",font=("Calibri",20))
+
+    gendertitle.grid(row=5, column=0)
+    #grid uses the matrix row column concepts to organize the widgets
+    
+    Gender= StringVar() 
+    #value holder for integer variables
+
+    male= Radiobutton(window, text='Male', variable=Gender, value='male', font=("Calibri",20))
+    female= Radiobutton(window, text='Female', variable=Gender, value='female', font=("Calibri",20))
+    nosay= Radiobutton(window, text='No say', variable=Gender, value='nosay', font=("Calibri",20))
+
+    male.grid(column=1, row=5)
+    female.grid(column=2, row=5)
+    nosay.grid(column=3, row=5)
+
+    #********************************************************
+    #Height in Meter, for user in cm
+    #creating text label and input label
+
+
+    heightlabel=StringVar()
+    #value holder for string variables
+
+    heightlabel.set("Height in cm:")
+    #set the variable to the VALUE
+
+    heighttitle=Label(window, textvariable=heightlabel,bg="yellow",fg="green",font=("Calibri",20))
+
+    heighttitle.grid(row=7, column=0)
+    #grid uses the matrix row column concepts to organize the widgets
+
+    Height= IntVar() 
+    #value holder for integer variables
+
+    height= Entry(window)
+    height.grid(row=7, column=1)
+
+
+    #********************************************************
+    #Weight in Kg
+    #creating text label and input label
+
+    weightlabel=StringVar()
+    #value holder for string variables
+
+    weightlabel.set("Weight in Kg:")
+    #set the variable to the VALUE
+
+    weighttitle=Label(window, textvariable=weightlabel,bg="yellow",fg="green",font=("Calibri",20))
+
+    weighttitle.grid(row=8, column=0)
+    #grid uses the matrix row column concepts to organize the widgets
+
+    Weight= IntVar() 
+    #value holder for integer variables
+
+    weight= Entry(window)
+    weight.grid(row=8, column=1)
+
+    
+    #***************************************************
+    #BMI Generate
+
+
+    btn=Button(window,text="BMI Generate",bg="orange",fg="red",command=calc_BMI ,font=("Calibri",20))
+    #bg and fg changing the background and foreground colour
+
+    btn.grid(row=9,column=0)
+
+    bmi_label=Label(window,font=("Calibri",30))
+    #setting the label font
+    bmi_label.grid(row=9,column=1) 
+    
+
+    window.mainloop()
+
+input_value()
