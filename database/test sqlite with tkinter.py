ab1e6ac4f4b87a24d0d6d40e84e26b9936bd58b9
MaagMichael <63949690+MaagMichael@users.noreply.github.com>
2021-02-14 21:23:34 +0100

    x

--- database/test sqlite with tkinter.py
+++ database/test sqlite with tkinter.py
@@ -1,12 +1,12 @@
 import sqlite3 
 import tkinter as tk
 
-root = tk.Tk()
+root = tk.Tk() 
 
 conn = sqlite3.connect('./database/user.db')
 c = conn.cursor()
 
-# create table "user" for account opening
+# create table "user"
 c.execute("""CREATE TABLE IF NOT EXISTS user ( 
             name text,
             dateofbirth text
@@ -17,26 +17,45 @@ c.execute("""CREATE TABLE IF NOT EXISTS user (
 #         c.execute("INSERT INTO user VALUES (:name, :weight, :height, :gender)", {'name':value_in.name, 'weight':value_in.weight, 'height':value_in.height, 'gender':value_in.gender})
 
 def store_sql(value_in):
+    print(value_in)
     with conn:
-        c.execute("INSERT INTO user VALUES (?, ?, ?, ?)", (value_in))
+        c.execute("INSERT INTO user VALUES (?, ?)", (value_in))
+        # conn.commit() 
+         
 
-if __name__ == "__main__":
+def reg_mask():
+    # window of input mask
+    canvas = tk.Canvas(root, width=500, height=300)
+    canvas.grid(columnspan=4, rowspan=4)
 
-    canvas = tk.Canvas(root, width=400, height=300)
-    canvas.grid(columnspan=4, rowspan=3)
+    # label and entry fields
+    name_label = tk.Label(root, text = "Your Name here:")
+    name_label.grid(column=1, row=0)
+    user_name = tk.Entry(root, width = 30)
+    user_name.grid(column=2, row=0)
+    dateofbirth_label = tk.Label(root, text = "Your date of birth (YYYY-MM-DD):")
+    dateofbirth_label.grid(column=1, row=1)
+    user_dateofbirth = tk.Entry(root, width = 30)
+    user_dateofbirth.grid(column=2, row=1)
 
+    dataset = (user_name, user_dateofbirth)
+    print(dataset)
 
-    # name_in = str(input("your name is ? "))
-    # weight_in = float(input("your weigt in Kg is ? "))
-    # height_in = float(input("your height in m is ? "))
-    # gender_in = str(input("your gender is male/female ? "))
-    # input_user = (name_in, weight_in, height_in, gender_in)
-    
-    # store_sql(input_user)
+    #register button
+    reg_text = tk.StringVar()
+    reg_btn = tk.Button(root, textvariable=reg_text, command=lambda:store_sql(dataset), font="Raleway", bg="#20bebe", fg="white", height=2, width=15)
+    reg_text.set("Register")
+    reg_btn.grid(column=2, row=3)
     
     # conn.close()
 
     root.mainloop()
+
+
+if __name__ == "__main__":
+    reg_mask()
+
+    
 
     # https://www.pythoncentral.io/introduction-to-sqlite-in-python/
     # check content of db here https://sqliteonline.com/
