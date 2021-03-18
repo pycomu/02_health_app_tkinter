5d1ee696e7f493d4666bb61047b4bd11e22a97f0
MaagMichael <63949690+MaagMichael@users.noreply.github.com>
2021-03-18 20:20:24 +0100

    First App running

--- Main_BMI.py
+++ Main_BMI.py
@@ -1,6 +1,10 @@
 # import dependencies
 import tkinter as tk
 from tkinter import ttk
+# from tkcalendar import *
+# from tkcalendar import Calendar
+# import tkinter.messagebox
+#from tkcalendar import Calendar, DateEntry
 import sqlite3
 
 import pandas as pd
@@ -53,7 +57,7 @@ class health_app(tk.Tk):
                 
         tk.Tk.__init__(self, *args, **kwargs)        
         self.wm_title("Health Tracker")
-        self.geometry("500x500")
+        self.geometry("600x800")
         
         container = tk.Frame(self) 
         container.pack(side = "top", fill = "both", expand = True) 
@@ -61,7 +65,7 @@ class health_app(tk.Tk):
         container.grid_columnconfigure(0, weight = 1)
         
         self.frames = {}  
-        for F in (LoginPage,RegisterPage, MainPage, BMI_chart):  # define names of 2 frames
+        for F in (LoginPage,RegisterPage, MainPage, ChildPage, ChartPage):  # define names of 2 frames
             frame = F(container, self)  
             self.frames[F] = frame
             frame.grid(row = 0, column = 0, sticky ="nsew") # each frame has the same grid parameters for layout
@@ -80,7 +84,7 @@ class LoginPage(tk.Frame):
     def __init__(self, parent, controller): # controller is "child" of class health_app to call its functions
         tk.Frame.__init__(self, parent)
         
-        label = ttk.Label(self, text ="Main Page", font="bold")        
+        label = ttk.Label(self, text ="Login Page", font="bold")        
         label.grid(row = 0, column = 1, padx = 10, pady = 10)
   
         button1 = ttk.Button(self, text ="Login", command = lambda : controller.show_frame(MainPage))
@@ -95,7 +99,7 @@ class LoginPage(tk.Frame):
         button4 = ttk.Button(self, text ="Export", command = lambda : controller.show_frame(Export))
         button4.grid(row = 3, column = 4, padx = 10, pady = 10)
 
-        label1 = ttk.Label(self, text="PIN", font=("Arial Italic", 10))
+        label1 = ttk.Label(self, text="PIN (4 digits)", font=("Arial Italic", 10))
         label1.grid(row=4, column=1)
 
         Entry1 = ttk.Entry(self,width=10, show="*")  # field entry for PIN
@@ -115,7 +119,7 @@ class RegisterPage(tk.Frame):
         label2 = ttk.Label(self, text="Account Name", font=("Arial Italic", 10))
         label2.grid(row=2,column=1)
         
-        label3 = ttk.Label(self, text="Enter PIN", font=("Arial Italic", 10))
+        label3 = ttk.Label(self, text="Enter PIN (4 digits)", font=("Arial Italic", 10))
         label3.grid(row=3,column=1)
 
         label4 = ttk.Label(self, text="Re-enter PIN", font=("Arial Italic", 10))
@@ -140,17 +144,28 @@ class RegisterPage(tk.Frame):
         button2 = ttk.Button(self, text ="Cancel", command = lambda : controller.show_frame(LoginPage)) 
         button2.grid(row = 5, column = 2, padx = 10, pady = 10) 
 
+class ChildPage(tk.Frame):       
+    def __init__(self, parent, controller): # controller is "child" of class health_app to call its functions
+        tk.Frame.__init__(self, parent)
+        
+        label = ttk.Label(self, text ="Child Registration Page", font="bold")        
+        label.grid(row = 0, column = 1, padx = 10, pady = 10)
+
+        button1 = ttk.Button(self, text ="Close", command = lambda : controller.show_frame(MainPage)) 
+        button1.grid(row = 5, column = 1, padx = 10, pady = 10)
+
+
 class MainPage(tk.Frame):      
     def __init__(self, parent, controller): 
         tk.Frame.__init__(self, parent)
         
-        label1 = ttk.Label(self, text ="USer Details - Main",font="bold")        
+        label1 = ttk.Label(self, text ="USer Details - Main Page",font="bold")        
         label1.grid(row = 0, column = 0, padx = 10, pady = 10)
 
-        label2 = ttk.Label(self, text="Height", font=("Arial Italic", 10))
+        label2 = ttk.Label(self, text="Height in m", font=("Arial Italic", 10))
         label2.grid(column=0, row=2)
         
-        label3 = ttk.Label(self, text="Weight", font=("Arial Italic", 10))
+        label3 = ttk.Label(self, text="Weight in Kg", font=("Arial Italic", 10))
         label3.grid(column=0, row=3)
 
         label4 = ttk.Label(self, text="Age", font=("Arial Italic", 10))
@@ -174,7 +189,7 @@ class MainPage(tk.Frame):
         self.list.insert(0, *list_child) 
         self.list.grid(row=1,column=1)
                                    
-        button2 = ttk.Button(self, text="BMI Chart", command=lambda: BMI_chart())
+        button2 = ttk.Button(self, text="BMI Chart", command = lambda: controller.show_frame(ChartPage))
         button2.grid(row = 6, column = 1,padx = 10, pady = 10) 
 
         button5 = ttk.Button(self, text ="Close", command = lambda : controller.show_frame(LoginPage)) 
@@ -182,15 +197,18 @@ class MainPage(tk.Frame):
 
         button1 = ttk.Button(self, text="Calculate BMI", command=lambda:  calc_bmi())
         button1.grid(row = 6, column = 0,padx = 10, pady = 10) 
+
+        button3 = ttk.Button(self, text="Register Child", command = lambda: controller.show_frame(ChildPage))
+        button3.grid(row = 2, column = 3,padx = 10, pady = 10) 
         
         
         def calc_bmi(): # +++++++++++++++++++++++++++++++ calculation to be changed !
          height = Entry1.get()
          if height:
-          height = int(height)
+          height = float(height)
          weight = Entry2.get()
          if weight:
-            weight = int(weight)
+            weight = float(weight)
          BMI =  weight/(height*2)
          
          label5 = ttk.Label(self, text="BMI Index", font=("Arial Italic", 10))
@@ -199,10 +217,12 @@ class MainPage(tk.Frame):
          label6 = ttk.Label(self, text=BMI, font=("Arial Italic", 10))
          label6.grid(column=1, row=5)
 
-class BMI_chart(tk.Frame):      
+class ChartPage(tk.Frame):      
     def __init__(self, parent, controller): 
         tk.Frame.__init__(self, parent)
 
+        label1 = ttk.Label(self, text ="BMI Chart Page",font="bold")        
+        label1.grid(row = 0, column = 0, padx = 10, pady = 10)
         
         load = Image.open("./growthchart_example2.gif")
         #img = ImageTk.PhotoImage(Image.open("growthchart_example2.gif")) 
@@ -210,12 +230,12 @@ class BMI_chart(tk.Frame):
         #canvas.create_image(250, 250, image=img) 
         img = ttk.Label(self, image=render)
         img.image = render
-        img.pack()
+        img.grid(column=1, row=1)
 
-        button = Button (self, text="Close.", command = lambda:  controller.show_frame(MainPage))
-        button.pack()
-        button = Button (self, text="Export") # go to export page
-        button.pack()
+        button = ttk.Button (self, text="Close.", command = lambda:  controller.show_frame(MainPage))
+        button.grid(column=1, row=3)
+        button = ttk.Button (self, text="Export") # go to export page
+        button.grid(column=1, row=5)
         
 
 # ++++++ functions for database modifications e.g. insert data, delete data, update data
