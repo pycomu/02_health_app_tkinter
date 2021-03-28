40646f9d8c2082019d89682c0f99e28fe91e3ccb
MaagMichael <63949690+MaagMichael@users.noreply.github.com>
2021-03-28 17:30:16 +0200

    own library

---
+++ library/example_db.py
@@ -0,0 +1,9 @@
+def add_2_fig(x,y):
+    sum = x+y
+    return sum
+
+def add_geeting(name):
+    return "Hello " + name + " and welcome"
+
+
+
---
+++ library/main.py
@@ -0,0 +1,5 @@
+# import example_db
+from example_db import *
+
+print(add_2_fig(10,20))
+print(add_geeting("Memoona"))
